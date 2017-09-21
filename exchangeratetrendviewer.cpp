f2a53411528da3b4e235f6f6dab826685fd1bd92
alvaro83 <alvaro.g.arroyo@gmail.com>
2017-09-21 14:57:42 +0200

    Implementation of button clicked slot
    Allow user to save exchange rate trend chart as image (PNG, JPEG, TIFF)
    in selected disk location.

--- exchangeratetrendviewer.cpp
+++ exchangeratetrendviewer.cpp
@@ -1,5 +1,8 @@
 #include "exchangeratetrendviewer.h"
 #include "ui_exchangeratetrendviewer.h"
+#include <QMessageBox>
+#include <QPixmap>
+#include <QFileDialog>
 
 ExchangeRateTrendViewer::ExchangeRateTrendViewer(QWidget *parent) :
     QDialog(parent),
@@ -15,5 +18,19 @@ ExchangeRateTrendViewer::~ExchangeRateTrendViewer()
 
 void ExchangeRateTrendViewer::on_saveButton_clicked()
 {
-    //TODO
+    QString saveFilename = QFileDialog::getSaveFileName(this, "Save as", "Choose a filename", "PNG(*.png);; TIFF(*.tiff *.tif);; JPEG(*.jpg *.jpeg)");
+
+    if (saveFilename != NULL)
+    {
+        QString saveExtension = "PNG";
+        int pos = saveFilename.lastIndexOf('.');
+
+        if (pos >= 0)
+            saveExtension = saveFilename.mid(pos + 1);
+
+        if(!QPixmap::grabWidget(ui->rateViewer).save(saveFilename, qPrintable(saveExtension)))
+        {
+            QMessageBox::warning(this, "Warning", "File could not be saved", QMessageBox::Ok);
+        }
+    }
 }
